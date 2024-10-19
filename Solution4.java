aef1944e68aea837ae59b89b489a0daf553f3a14
kathy <2752109281@qq.com>
2024-10-19 11:20:36 +0800

    Add files via upload

--- Solution4.java
+++ Solution4.java
@@ -2,68 +2,51 @@ import java.util.Arrays;
 
 /**
  * @description:
- *
- * 给定一个无序的数组 nums，返回 数组在排序之后，相邻元素之间最大的差值 。如果数组元素个数小于 2，则返回 0 。
+ * 给定一个无序的数组 nums，返回数组在排序之后，相邻元素之间的最大差值。 
+ * 如果数组元素个数小于 2，则返回 0。
  *
  * 您必须编写一个在「线性时间」内运行并使用「线性额外空间」的算法。
- *
- *
- *
- * 示例 1:
- *
- * 输入: nums = [3,6,9,1]
- * 输出: 3
- * 解释: 排序后的数组是 [1,3,6,9], 其中相邻元素 (3,6) 和 (6,9) 之间都存在最大差值 3。
- * 示例 2:
- *
- * 输入: nums = [10]
- * 输出: 0
- * 解释: 数组元素个数小于 2，因此返回 0。
- *
- *
- * 提示:
- *
- * 1 <= nums.length <= 105
- * 0 <= nums[i] <= 109
- *
  */
 class Solution4 {
     public int maximumGap(int[] nums) {
 
         int n = nums.length;
-        int ret = 0;
-
-        //数组元素个数小于 2，因此返回 0。
+        
         if (n < 2) {
             return 0;
         }
-        // long exp = 1;
-        // int[] buf = new int[n];
-        // //找出数组中的最大值
-        // int maxVal = Arrays.stream(nums).max().getAsInt();
         
-        // while (maxVal > exp) {
-        //     int[] cnt = new int[10];
-        //     for (int i = 0; i < n; i++) {
-        //         int digit = (nums[i] / (int) exp) % 10;
-        //         cnt[digit]++;
-        //     }
-        //     for (int i = 1; i < 10; i++){
-        //         cnt[i] += cnt[i - 1];
-        //     for (int j = n - 1; j >= 0; j--) {
-        //         int digit = (nums[j] / (int) exp) % 10;
-        //         buf[cnt[digit] - 1] = nums[j];
-        //         cnt[digit]--;
-        //     }
-        //     System.arraycopy(buf, 0, nums, 0, n);
-        //     exp += 10;
-        //     }
-        // }
-        Arrays.sort(nums);
-        //遍历数组，比较相邻两数间的差值
-                for (int i = 1; i < n; i++) {
-                ret = Math.max(ret, nums[i] - nums[i - 1]);
+        int minVal = Integer.MAX_VALUE, maxVal = Integer.MIN_VALUE;
+        for (int num : nums) {
+            minVal = Math.min(minVal, num);
+            maxVal = Math.max(maxVal, num);
+        }
+        
+        int bucketSize = Math.max(1, (maxVal - minVal) / (n - 1)); 
+        int bucketCount = (maxVal - minVal) / bucketSize + 1;
+        
+        int[] minBucket = new int[bucketCount];
+        int[] maxBucket = new int[bucketCount];
+        Arrays.fill(minBucket, Integer.MAX_VALUE);  
+        Arrays.fill(maxBucket, Integer.MIN_VALUE);
+        
+        for (int num : nums) {
+            int idx = (num - minVal) / bucketSize;
+            minBucket[idx] = Math.min(minBucket[idx], num);
+            maxBucket[idx] = Math.max(maxBucket[idx], num);
+        }
+        
+        int prevMax = minVal;
+        int ret = 0;
+        for (int i = 0; i < bucketCount; i++) {
+            if (minBucket[i] == Integer.MAX_VALUE) {
+                continue;
             }
+
+            ret = Math.max(maxGap, minBucket[i] - prevMax);
+            prevMax = maxBucket[i];
+        }
+
         return ret;
     }
 }
